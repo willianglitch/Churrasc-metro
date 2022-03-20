5361496d5f483b78388e057fce33058216c89763
Willian da silva soares <90284411+willianglitch@users.noreply.github.com>
2022-03-20 16:59:41 -0300

    first commit
    organizando e construindo o código

---
+++ script.js
@@ -0,0 +1,8 @@
+// quantas quantas quilos de carne um adulto conseguer comer
+
+// Para os homens, calcule 450 g por pessoa. Para as mulheres, 
+// 300 g por pessoa. Já para as crianças, 200 g são suficientes.
+
+// cerveja 1400ml por pessoa , passou da hora inicial 5 horas+ 2200ml
+// refrigerante 1000ml/litro por pessoa  , + 5 horas 1500ml
+//criança é o meio desse cálculo
