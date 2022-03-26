6bff0b9d215a69e387609ece78296382307447b3
Willian da silva soares <90284411+willianglitch@users.noreply.github.com>
2022-03-26 17:15:35 -0300

    update 1.5
    javascript

--- script.js
+++ script.js
@@ -6,3 +6,30 @@
 // cerveja 1400ml por pessoa , passou da hora inicial 5 horas+ 2200ml
 // refrigerante 1000ml/litro por pessoa  , + 5 horas 1500ml
 //criança é o meio desse cálculo
+
+const homenAdultos = document.getElementById("homens")
+const mulherAdultas = document.getElementById("mulheres")
+const inputCrianças = document.getElementById("criança")
+const tempoDuração  = document.getElementById("duração")
+
+let resultado = document.getElementById("resultado")
+
+function CÁLCULAR(){
+    let homens = homenAdultos.value;
+    let mulheres = mulherAdultas.value;
+    let crianças = inputCrianças.value;
+
+
+    let calcH = 450 * homens;
+    let calcM = 300 * mulheres;
+    let calcC = 225 * crianças
+
+    const carneTotal = calcH + calcM + calcC;
+
+
+}
+
+
+
+
+
